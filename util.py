bdd108976ac41745034a020c15eab1b2e1644ed6
Sven <github@svenva.de>
2022-10-10 17:25:09 +0000

    pre commit hooks with black

--- util.py
+++ util.py
@@ -3,17 +3,11 @@ import pandas as pd
 import numpy as np
 
 
-
 def snapshot_api(query, params=None):
-    ENDPOINT = 'https://hub.snapshot.org/graphql'
-    response = requests.post(ENDPOINT,
-                            headers={                      
-                                'accept': 'application/json'
-                            },
-                            params={
-                                'query': query
-                            })
+    ENDPOINT = "https://hub.snapshot.org/graphql"
+    response = requests.post(
+        ENDPOINT, headers={"accept": "application/json"}, params={"query": query}
+    )
 
     print(response)
-    return response.json()['data']
-
+    return response.json()["data"]
