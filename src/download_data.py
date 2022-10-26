1c7f685affe90a89a4a86526c7a6670162433e6c
Sven <github@svenva.de>
2022-10-26 10:34:46 +0000

    imports

---
+++ src/download_data.py
@@ -0,0 +1,13 @@
+# %%
+import time
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
+from tqdm import tqdm
+import json
+
+projectfolder = Path("/project/IS808_adv_DS_lab")
+# %%
