85f78311bd3f4060286d8570bf18b4e65eef66a3
Sven <github@svenva.de>
2022-11-04 20:36:11 +0000

    with counter and export

--- src/i021shared_dao_membership.py
+++ src/i021shared_dao_membership.py
@@ -27,86 +27,111 @@ from tqdm import tqdm
 import dask.dataframe as dd
 import itertools
 import collections
+import pickle
 
 # %%
 data_dir = projectfolder / "data"
 dao_voter_mapping = "dao_voter_mapping.pq"
 opensea_downloads = "opensea_collections.pq"
+shared_daos_between_voters_pickle = "shared_daos_between_voters.pickle"
+shared_daos_between_voters = "shared_daos_between_voters.pq"
 
-minimum_number_of_votes = 10
+minimum_number_of_votes = 15
 minimum_number_of_nfts = 10
 
 # %%
-voters_with_nfts = (
-    pd.read_parquet(data_dir / opensea_downloads, columns=["requestedaddress", "slug"])
-    .drop_duplicates()
-    .groupby("requestedaddress")
-    .size()
-)
-voters_with_nfts = set(
-    voters_with_nfts[voters_with_nfts > minimum_number_of_nfts].index
-)
+if not (data_dir / shared_daos_between_voters_pickle).is_file():
+    print("Rerunning creation")
+    voters_with_nfts = (
+        pd.read_parquet(
+            data_dir / opensea_downloads, columns=["requestedaddress", "slug"]
+        )
+        .drop_duplicates()
+        .groupby("requestedaddress")
+        .size()
+    )
+    voters_with_nfts = set(
+        voters_with_nfts[voters_with_nfts > minimum_number_of_nfts].index
+    )
 
-df_dao_voters = pd.read_parquet(
-    data_dir / dao_voter_mapping, columns=["dao", "voter", "proposalid"]
-).drop_duplicates()
+    df_dao_voters = pd.read_parquet(
+        data_dir / dao_voter_mapping, columns=["dao", "voter", "proposalid"]
+    ).drop_duplicates()
 
-# Get the number of proposals for which a voter voted
+    # Get the number of proposals for which a voter voted
 
-nproposals = df_dao_voters.groupby(["voter"]).size()
-voters_with_enough_votes = set(nproposals[nproposals >= minimum_number_of_votes].index)
+    nproposals = df_dao_voters.groupby(["voter"]).size()
+    voters_with_enough_votes = set(
+        nproposals[nproposals >= minimum_number_of_votes].index
+    )
 
-relevant_voters = voters_with_nfts & voters_with_enough_votes
+    relevant_voters = voters_with_nfts & voters_with_enough_votes
 
-print(
-    f"Number of voters: {len(relevant_voters)} => {len(relevant_voters)**2/2/1e9:.2f} billion combinations"
-)
+    print(
+        f"Number of voters: {len(relevant_voters)} => {len(relevant_voters)**2/2/1e6:.2f} million combinations"
+    )
 
-df_by_voters = df_dao_voters.drop("proposalid", axis=1).set_index("voter").sort_index()
-df_by_voters = df_by_voters.loc[df_by_voters.index.isin(relevant_voters)]
+    # Reduce the voters to the relevant ones
+    df_by_voters = (
+        df_dao_voters.drop("proposalid", axis=1).set_index("voter").sort_index()
+    )
+    df_by_voters = df_by_voters.loc[df_by_voters.index.isin(relevant_voters)]
 
-lookup_dict = (
-    df_by_voters.groupby("voter").agg({"dao": lambda x: set(x)}).iloc[:, 0].to_dict()
-)
-# del df_by_voters, voters_with_nfts, voters_with_enough_votes, relevant_voters
+    # Create the main iteration input dictionary
+    lookup_dict = (
+        df_by_voters.reset_index()
+        .groupby("dao")
+        .agg({"voter": lambda x: set(x)})
+        .iloc[:, 0]
+        .to_dict()
+    )
+    del (
+        df_dao_voters,
+        df_by_voters,
+        voters_with_nfts,
+        voters_with_enough_votes,
+        relevant_voters,
+    )
+
+    counter = collections.Counter()
+
+    # Iterate over all DAOs sorted by size (number of addresses); largest ones first
+    for dao in tqdm(
+        sorted(lookup_dict, key=lambda k: len(lookup_dict[k]), reverse=True)
+    ):
+
+        # Get the voters for the given DAO
+        voters = lookup_dict[dao]
+
+        # Sorted will force the right order for the keys, so that we don't get both A,B and B,A
+        for voter, othervoter in itertools.combinations(sorted(voters), 2):
+
+            # Add 1 to the voter pair
+            counter.update([(voter, othervoter)])
+
+    # Dump the results into a file to preserve data if running out of memory
+    with open(data_dir / shared_daos_between_voters_pickle, "wb") as f:
+        pickle.dump(counter, f)
+else:
+    print("Loading existing pickle")
+    with open(data_dir / shared_daos_between_voters_pickle, "rb") as f:
+        counter = pickle.load(f)
+
+
 # %%
-_coll = {}
-counter = collections.Counter()
-for voter, daos in tqdm(lookup_dict.items()):
+if not (data_dir / shared_daos_between_voters).is_file() or True:
+    print("Loading into DataFrame")
+    df = pd.DataFrame.from_dict(
+        counter, orient="index", columns=["nshareddaos"]
+    ).reset_index()
+    df[["voter1", "voter2"]] = pd.DataFrame(df["index"].tolist(), index=df.index)
+    df = df.drop("index", axis=1).loc[:, ["voter1", "voter2", "nshareddaos"]]
 
-    for othervoter in lookup_dict.keys():
-
-        key = tuple(sorted([voter, othervoter]))
-
-        if othervoter != voter and not key in _coll.keys():
-            otherdaos = lookup_dict[othervoter]
-
-            n_shared_daos = len(daos & otherdaos)
-
-            _coll[key] = n_shared_daos
-
-
+    print("Exporting to parquet")
+    df.to_parquet(
+        data_dir / shared_daos_between_voters, compression="brotli", index=False
+    )
+    (data_dir / shared_daos_between_voters_pickle).unlink()
 # %%
-pd.DataFrame(_coll)
-# %%
-sys.getsizeof(_coll) / 1e6
-# %%
-len(lookup_dict.keys()) ** 2 / 2 / 1e9
-# %%
-len(voters_with_nfts), len(voters_with_enough_votes), len(
-    voters_with_nfts & voters_with_enough_votes
-)
-
-# %%
-nproposals[nproposals >= 3]
-# %%
-nproposals.value_counts().sort_index().iloc[:10].plot.line(logy=False)
-# %%
-dd.read_parquet(data_dir / opensea_downloads).head()
-# %%
-
-# %%
-counter = collections.Counter()
-# %%
-counter.update([("a", "b")])
+df["nshareddaos"].mean()
 # %%
