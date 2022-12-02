aef2eea29d0948df1de7526b03f18effe77141a1
Sven <github@svenva.de>
2022-12-02 19:34:45 +0000

    plot sensitivity charts

--- src/i018relevant_voters.py
+++ src/i018relevant_voters.py
@@ -21,6 +21,9 @@ sys.path.insert(0, "")  # Required for loading modules
 
 
 import pandas as pd
+from tqdm import tqdm
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Gets or creates a logger
 import logging
@@ -57,95 +60,95 @@ relevant_voters_with_voterid = "relevant_voters_with_voterid.pq"
 
 
 def get_relevant_voters(
-    list_of_voters: list = [],
-    minimum_number_of_votes=0,
-    maximum_number_of_nfts=100_000_000,
-    minimum_number_of_nfts=0,
     nft_projects=None,
-    votes_after_date="2019-01-01",
+    **kwargs,
 ):
     """
-    Returns the input dictionary required for the next steps.
-    If no list_of_voters is passed, it will apply some default filters.
-    Otherwise it will use the provided list of voters without further limitation.
+    Generates the relevant voters given some restrictions
     """
 
+    # Load dataframes from disk
     df_dao_voters = pd.read_parquet(
         data_dir / dao_voter_mapping,
         columns=["dao", "voterid", "proposalid", "timestamp"],
     ).drop_duplicates(subset=["dao", "voterid", "proposalid"])
 
-    if len(list_of_voters) == 0:
+    nft_data = pd.read_parquet(
+        data_dir / opensea_downloads, columns=["voterid", "slug"]
+    ).drop_duplicates()
+    voters_with_nfts = nft_data.groupby("voterid").size()
 
-        logger.info(
-            f"Min NFT: {minimum_number_of_nfts} Min votes: {minimum_number_of_votes} File provided: {nft_projects is not None}"
-        )
+    if nft_projects is not None:
 
-        nft_data = pd.read_parquet(
-            data_dir / opensea_downloads, columns=["voterid", "slug"]
-        ).drop_duplicates()
+        nft_projects_df = pd.read_parquet(data_dir / nft_projects)
 
-        voters_with_nfts = nft_data.groupby("voterid").size()
-        voters_with_nfts = set(
-            voters_with_nfts[
-                (
-                    (voters_with_nfts > minimum_number_of_nfts)
-                    & (voters_with_nfts <= maximum_number_of_nfts)
-                )
-            ].index
-        )
+    return get_relevant_voters_given_data(
+        voters_with_nfts=voters_with_nfts,
+        nft_data=nft_data,
+        nft_projects_df=nft_projects_df,
+        df_dao_voters=df_dao_voters,
+        **kwargs,
+    )
 
-        if (
-            nft_projects is not None
-            and isinstance(nft_projects, str)
-            and nft_projects.endswith("csv")
-        ):
-            voters_with_specific_nft_projects = set(
-                pd.merge(
-                    nft_data,
-                    pd.read_csv(data_dir / nft_projects),
-                    on="slug",
-                    how="inner",
-                )["voterid"]
+
+def get_relevant_voters_given_data(
+    minimum_number_of_votes=0,
+    maximum_number_of_nfts=100_000_000,
+    minimum_number_of_nfts=0,
+    votes_after_date="2019-01-01",
+    voters_with_nfts=None,
+    nft_data=None,
+    nft_projects_df=None,
+    df_dao_voters=None,
+):
+    # Information
+    logger.info(
+        f"NFT: {minimum_number_of_nfts}-{maximum_number_of_nfts} Min votes: {minimum_number_of_votes} File provided: {nft_projects is not None}"
+    )
+
+    # Compute the set for number of NFTs
+    voters_with_nfts_set = set(
+        voters_with_nfts[
+            (
+                (voters_with_nfts >= minimum_number_of_nfts)
+                & (voters_with_nfts <= maximum_number_of_nfts)
             )
-        elif (
-            nft_projects is not None
-            and isinstance(nft_projects, str)
-            and nft_projects.endswith("pq")
-        ):
-            voters_with_specific_nft_projects = set(
-                pd.merge(
-                    nft_data,
-                    pd.read_parquet(data_dir / nft_projects),
-                    on="slug",
-                    how="inner",
-                )["voterid"]
-            )
-        elif nft_projects is not None:
-            raise ValueError("Something provided for nft_project, but unhandled type")
+        ].index
+    )
 
-        # Get the number of proposals for which a voter voted
-        nproposals = (
-            df_dao_voters.query(f"timestamp > '{votes_after_date}'")
-            .groupby(["voterid"])
-            .size()
-        )
-        voters_with_enough_votes = set(
-            nproposals[((nproposals >= minimum_number_of_votes))].index
+    # Compute the set for NFT projects
+    if nft_projects_df is not None:
+
+        voters_with_specific_nft_projects = set(
+            pd.merge(
+                nft_data,
+                nft_projects_df,
+                on="slug",
+                how="inner",
+            )["voterid"]
         )
 
-        if nft_projects is not None:
-            relevant_voters = (
-                voters_with_nfts
-                & voters_with_enough_votes
-                & voters_with_specific_nft_projects
-            )
-        else:
-            relevant_voters = voters_with_nfts & voters_with_enough_votes
+    # Get the number of proposals for which a voter voted
+    nproposals = (
+        df_dao_voters.query(f"timestamp > '{votes_after_date}'")
+        .groupby(["voterid"])
+        .size()
+    )
+    voters_with_enough_votes = set(
+        nproposals[((nproposals >= minimum_number_of_votes))].index
+    )
 
+    # Combine the sets into one set union
+    if nft_projects_df is not None:
+        relevant_voters = (
+            voters_with_nfts_set
+            & voters_with_enough_votes
+            & voters_with_specific_nft_projects
+        )
     else:
-        relevant_voters = set(list_of_voters)
+        relevant_voters = voters_with_nfts_set & voters_with_enough_votes
 
+    # Information
     logger.info(
         f"Number of voters: {len(relevant_voters)} => {len(relevant_voters)**2/2/1e6:.2f} million combinations"
     )
@@ -174,3 +177,53 @@ if __name__ == "__main__":
     rv.to_parquet(
         data_dir / relevant_voters_with_voterid, compression="brotli", index=False
     )
+
+    # %%
+
+    cd = {
+        "minimum_number_of_votes": range(1, 40, 3),
+        "minimum_number_of_nfts": range(1, 40, 3),
+        "maximum_number_of_nfts": range(100, 100_000, 5_000),
+        "votes_after_date": [
+            "2021-01-01",
+            "2022-01-01",
+            "2022-05-01",
+            "2022-08-01",
+        ],
+    }
+
+    default = {
+        "minimum_number_of_votes": 20,
+        "minimum_number_of_nfts": 20,
+        "maximum_number_of_nfts": 10000,
+        "votes_after_date": "2022-08-01",
+    }
+
+    _c = []
+    logger.setLevel(logging.WARN)
+    for var, values in cd.items():
+        for val in tqdm(values):
+            vars = {**default, **{var: val}}
+            relevant_voters = get_relevant_voters_given_data(
+                **vars,
+                voters_with_nfts=voters_with_nfts,
+                nft_data=nft_data,
+                nft_projects_df=nft_projects_df,
+                df_dao_voters=df_dao_voters,
+            )
+            _c.append(
+                {
+                    **vars,
+                    "variable": var,
+                    "number of relevant voters": len(relevant_voters),
+                }
+            )
+    df = pd.DataFrame(_c)
+    # %%
+    def plot_line(indf):
+        var = indf.name
+        g = sns.lineplot(indf, x=var, y="number of relevant voters")
+        g.set(ylim=(0, 20000))
+        plt.show()
+
+    df.groupby("variable").apply(plot_line)
