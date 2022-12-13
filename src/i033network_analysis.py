a457ec8883b3ce3822ac99ff4f73c1b22e8b1424
MeganSee <meganwmn@gmail.com>
2022-12-14 00:32:13 +0100

    failed to load node attributes in gexf file

--- src/i033network_analysis.py
+++ src/i033network_analysis.py
@@ -63,7 +63,10 @@ print (df_network_voter_nodes.dtypes)
 G_voter = nx.from_pandas_edgelist(
     df_network_voter_edges, source="voter1", target="voter2", edge_attr= True,
 )
-#  
+
+# nx.set_node_attributes(G_voter, pd.Series(df_network_voter_nodes.taste, index=df_network_voter_nodes.voterid).to_dict(), 'taste')
+
+#
 list(G_voter.nodes)
 list(G_voter.edges)
 G_voter.nodes
@@ -71,7 +74,6 @@ G_voter.nodes
 for i in sorted(G_voter.nodes()):
     G_voter.nodes[i]['taste'] = df_network_voter_nodes.taste[i]
 
-# nx.set_node_attributes(G_voter, pd.Series(df_network_voter_nodes.taste, index=df_network_voter_nodes.voterid).to_dict(), 'taste')
 node_colors = {
     'art': 'blue', 
     'domain-names': 'red', 
@@ -86,6 +88,12 @@ df_network_voter_nodes['node_color'] = df_network_voter_nodes['taste'].map(node_
 node_attr = df_network_voter_nodes.set_index('voterid').to_dict(orient = 'index')
 nx.set_node_attributes(G_voter, node_attr)
 
+
+nx.write_gexf(G_voter,f"{dir_path}/gragh_voter.gexf")
+with io.open(f"{dir_path}/gragh_voter.nx", mode="wb") as f:
+    pickle.dump(G_voter, f)
+
+# %%
 plt.figure(figsize=(6,6))
 nx.draw(G_voter,
     pos = nx.spring_layout(G_voter, weight = 'nshareddaos_y'),
@@ -96,11 +104,6 @@ nx.draw(G_voter,
     )
 plt.plot()
 plt.show()
-
-nx.write_gexf(G_voter,f"{dir_path}/gragh_voter.gexf")
-with io.open(f"{dir_path}/gragh_voter.nx", mode="wb") as f:
-    pickle.dump(G_voter, f)
-
 # %%
 net_voter = Network(height="1200px", width="100%", notebook=True)
 net_voter.repulsion()
